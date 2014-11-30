253dfa8ce1c0dc61cf9a47c76ec6aa09467f50f6
Pradeep <pradeep@incaendo.com>
2014-11-30 10:43:56 +0530

    add error class on error message

--- js/changePassword.js
+++ js/changePassword.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
   $('#change-password').click(function() {
     var password = $('#new-password').val();
     if (password == '') {
-      $('#error').html(Yii.t('js', "Please enter new Password"));
+      $('#error').removeClass('alert-success');
+      $('#error').html(Yii.t('js', "Please enter new Password")).addClass('alert-error');
       return false;
     }
     var confirmPassword = $('#confirm-password').val();
     if (confirmPassword == '') {
-      $('#error').html(Yii.t('js', "Please enter confirm password"));
+      $('#error').removeClass('alert-success');
+      $('#error').html(Yii.t('js', "Please enter confirm password")).addClass('alert-error');
       return false;
     }
     if (password != confirmPassword) {
-      $('#error').html(Yii.t('js', "Password does not match"));
+      $('#error').removeClass('alert-success');
+      $('#error').html(Yii.t('js', "Password does not match")).addClass('alert-error');
       return false;
     }
     $('#reset-password').submit();
