e6af657c168e05825e1110e96405cf8f34387687
Pradeep <pradeep@incaendo.com>
2014-12-19 18:28:57 +0530

    complete user statistics chart functionality

--- js/statistics.js
+++ js/statistics.js
@@ -1,53 +1,38 @@
+google.load("visualization", "1", {packages: ["corechart"]});
 $(document).ready(function() {
   $('.data-chart-type').click(function() {
     var chartData = $(this).val();
     if (chartData == '') {
       $('#line-chart').html('We are getting some error.');
       return false;
-    }
+    }   
     $.ajax({
       type: 'GET',
       url: statistic_page,
       dataType: 'json',
       data: {
         chart_data: chartData
-      },
+      },  
       success: function(resp) {
         if (resp.success) {
           drawChart(resp.data);
         } else {
           $('#line-chart').html('We are getting some error.');
-        }
-      },
+        }   
+      },  
       error: function(resp) {
         $('#line-chart').html('We are getting some error.');
-      }
-    });
-  });
+      }   
+    }); 
+  }); 
 });
 
-function drawChart(data) {
-  google.load("visualization", "1", {packages: ["corechart"]});
-  var options = {
-    title: data.title
-  };
-  new google.visualization.LineChart($('#line-chart')).draw(data, options);
-  var data = google.visualization.arrayToDataTable([
-    ['User', 'Age'],
-    ['10', 17.36],
-    ['20', 15.62],
-    ['30', 13.63],
-    ['40', '100'],
-    ['40', '90'],
-    ['40', '90'],
-    ['40', '90'],
-    ['40', '90'],
-    ['40', '90'],
-    ['40', '90'],
-    ['40', '90'],
-    ['40', '90'],
-    ['40', '80'],
-    ['40', '10']
-  ]);
-  new google.visualization.LineChart($('#line-chart')).draw(data, options);
+function drawChart(respData) {
+  data = respData.statistic_data;
+  var data = google.visualization.arrayToDataTable(data);
+  var options = { 
+     title: respData.title
+  };  
+  var chart = new google.visualization.LineChart(document.getElementById('line-chart'));
+  chart.draw(data, options);
 }
