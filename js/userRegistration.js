64dbd8a2199fdd152fd16ffdaec1c5e5cc016738
pradeep <pradeep@incaendo.com>
2014-12-30 15:28:35 +0530

    Added feature: Filter proposals based on tags. On Proposal viewing page.
    Also, corrected a check in Registration js file.

--- js/userRegistration.js
+++ js/userRegistration.js
@@ -8,10 +8,6 @@ $(document).ready(function() {
          $('#error').html(Yii.t('js',"Please enter Last Name")).css('color','red');
         return false;
     } 
-    if (password != confirmPassword) {
-      $('#error').html(Yii.t('js', "Password does not match")).css('color', 'red');
-      return false;
-    }
     var emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
     var email = $('#email').val();
     if (email == '') {
@@ -37,6 +33,10 @@ $(document).ready(function() {
     var confirmPassword = $('#confirm-password').val();
     if (confirmPassword == '') {
       $('#error').html(Yii.t('js', "Please enter confirm password")).css('color', 'red');
+      return false;
+    }
+    if (password != confirmPassword) {
+      $('#error').html(Yii.t('js', "Password does not match")).css('color', 'red');
       return false;
     }
     if ($('#register-terms').is(':checked') == false) {
