f9c30902ceb0eb2e8f15cbfadbdf57520bae76f7
Gian Paolo Mazzola <gian75@gmail.com>
2015-03-26 11:01:02 +0100

    Update message for browser compatibility check

--- js/jreject.config.js
+++ js/jreject.config.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
       msie: 8
     },
     header: '', // Header Text
-    paragraph1: 'Sito ottimizzato per browser di ultima generazione quali Chrome, Firefox, Safari; Internet Explorer versione 11 o superiore. Con versioni precedenti si potrebbero verificare problemi di visualizzazione.', // Paragraph 1  
+    paragraph1: 'Sito ottimizzato per browser di ultima generazione quali Chrome, Firefox, Safari; Internet Explorer versione 9 o superiore. Con versioni precedenti si potrebbero verificare problemi di visualizzazione.', // Paragraph 1  
     paragraph2: 'È preferibile aggiornare il browser a una versione più recente!',
     closeLink: 'Chiudi',
     closeMessage: '', // Message below close window link
