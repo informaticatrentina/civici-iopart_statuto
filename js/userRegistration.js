988b44c162af4419a7816b8b8d29aff3d98409ba
Pradeep <pradeep@incaendo.com>
2015-07-06 18:57:21 +0530

    Add changes for cookie alert box

--- js/userRegistration.js
+++ js/userRegistration.js
@@ -1,24 +1,37 @@
 $(document).ready(function() {
   $('#load-image').hide();
   $('#register-submit').click(function(){
-    if($('#firstname').val() == '') {
-         $('#error').html(Yii.t('js',"Please enter First Name")).css('color','red');
-        return false;
+    var textfieldRegExp = /^(([A-Za-z0-9]+ )*[A-Za-z0-9]+)+$/;
+    var firstname = $('#firstname').val();
+    if (firstname == '') {
+      $('#error').html(Yii.t('js','Please enter First Name')).css('color','red');
+      return false;
+    } else if (!textfieldRegExp.test(firstname)) {
+      $('#error').html(Yii.t('js', 'Please enter a valid First Name. Only alphanumeric chars and single spaces allowed!')).css('color','red');
+      return false;
     }
     if ($('#is-user').is(':checked')) {
-      if($('#lastname').val() == '') {
-         $('#error').html(Yii.t('js',"Please enter Last Name")).css('color','red');
+      var lastname = $('#lastname').val();
+      if (lastname == '') {
+        $('#error').html(Yii.t('js','Please enter Last Name')).css('color','red');
+        return false;
+      } else if (!textfieldRegExp.test(lastname)) {
+        $('#error').html(Yii.t('js', 'Please enter a valid Last Name. Only alphanumeric chars and single spaces allowed!')).css('color','red');
         return false;
       }
-      if($('#nickname').val() == '') {
-           $('#error').html(Yii.t('js',"Please enter Nickname")).css('color','red');
-          return false;
+      var nickname = $('#nickname').val();
+      if (nickname == '') {
+        $('#error').html(Yii.t('js','Please enter Nickname')).css('color','red');
+        return false;
+      } else if (!textfieldRegExp.test(nickname)){
+        $('#error').html(Yii.t('js', 'Please enter a valid Nickname. Only alphanumeric chars and single spaces allowed!')).css('color','red');
+        return false;
       }
     }
     var emailRegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
     var email = $('#email').val();
     if (email == '') {
-      $('#error').html(Yii.t('js',"Please enter email id")).css('color','red');
+      $('#error').html(Yii.t('js','Please enter email id')).css('color','red');
       return false;
     } else if (!emailRegExp.test(email)) {
       $('#error').html(Yii.t('js', 'Please enter valid email id')).css('color','red');
@@ -26,24 +39,24 @@ $(document).ready(function() {
     }
     var cemail = $('#cemail').val();
     if (cemail == '') {
-      $('#error').html(Yii.t('js',"Please enter same email id")).css('color','red');
+      $('#error').html(Yii.t('js','Please enter same email id')).css('color','red');
       return false;
     } else if (email != cemail) {
-      $('#error').html(Yii.t('js',"Please enter same email id")).css('color','red');
+      $('#error').html(Yii.t('js','Please enter same email id')).css('color','red');
       return false;
     }
     var password = $('#password').val();
     if (password == '') {
-      $('#error').html(Yii.t('js', "Please enter Password")).css('color', 'red');
+      $('#error').html(Yii.t('js', 'Please enter Password')).css('color', 'red');
       return false;
     }
     var confirmPassword = $('#confirm-password').val();
     if (confirmPassword == '') {
-      $('#error').html(Yii.t('js', "Please enter confirm password")).css('color', 'red');
+      $('#error').html(Yii.t('js', 'Please enter confirm password')).css('color', 'red');
       return false;
     }
     if (password != confirmPassword) {
-      $('#error').html(Yii.t('js', "Password does not match")).css('color', 'red');
+      $('#error').html(Yii.t('js', 'Password does not match')).css('color', 'red');
       return false;
     }
     if ($('#register-terms').is(':checked') == false) {
@@ -57,32 +70,38 @@ $(document).ready(function() {
     $('#error').html();
   });
 
-  $('#check_availability').on('click', function() {
+    $('#check_availability').on('click', function() {
+    var textfieldRegExp = /^(([A-Za-z0-9]+ )*[A-Za-z0-9]+)+$/;
     var nickname = $('#nickname').val();
     if (nickname.length > 0) {
-      $('#load-image').show();
-      $.ajax({
-        type: 'GET',
-        url: 'user/checknickname',
-        dataType: 'json',
-        data: {
-          nickname : nickname
-        },
-        success: function(resp) {
-          if (resp.success) {
-            $('#error').html(resp.msg).css('color','red');
-          } else {
-            $('#error').html(resp.msg).css('color','green');
+      if (!textfieldRegExp.test(nickname)){
+        $('#error').html(Yii.t('js', 'Please enter a valid Nickname. Only alphanumeric chars and single spaces allowed!')).css('color','red');
+        return false;
+      } else {
+        $('#load-image').show();
+        $.ajax({
+          type: 'GET',
+          url: 'user/checknickname',
+          dataType: 'json',
+          data: {
+            nickname : nickname
+          },
+          success: function(resp) {
+            if (resp.success) {
+              $('#error').html(resp.msg).css('color','red');
+            } else {
+              $('#error').html(resp.msg).css('color','green');
+            }
+            $('#load-image').hide();
+          },
+          error: function() {
+            $('#load-image').hide();
+            alert(Yii.t('js', 'Some error occured, please try again'));
           }
-          $('#load-image').hide();
-        },
-        error: function() {
-          $('#load-image').hide();
-          alert(Yii.t('js', 'Some error occured, please try again'));
-        }
-      });
+        });
+      }
     }
-    $('#error').html('');
+    $('#error').html(Yii.t('js','Please enter Nickname')).css('color','red');
   });
 
   $('#is-user, #is-org').on('change', function() {
